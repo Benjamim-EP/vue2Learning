de0f3a9ff880255aa6e46762ab8ba0c042e3b344
Benjamim-EP <bannk18@gmail.com>
2022-12-12 17:36:30 -0300

    adicionar fonte

--- calculadora/src/App.vue
+++ calculadora/src/App.vue
@@ -11,5 +11,26 @@ export default {
 </script>
 
 <style>
+@font-face{
+    font-family: "RobotoMono";
+    src: url("./fonts/RobotoMono-VariableFont_wght.ttf");
+}
+* {
+  font-family: "RobotoMono",monospace;
+}
+body {
+  margin:0;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 
+  color:rgb(8, 8, 8);
+  background-color: bisque;
+
+}
 </style>
